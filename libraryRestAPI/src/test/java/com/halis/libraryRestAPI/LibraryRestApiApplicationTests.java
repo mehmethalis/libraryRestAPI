4b5cad4bbfa6444fbdd8ff0df64329827b878f9b
Mehmet Halis <mehmethaliscicek@gmail.com>
2020-08-27 16:06:39 +0300

    initialized project with spring boot

---
+++ libraryRestAPI/src/test/java/com/halis/libraryRestAPI/LibraryRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halis.libraryRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
